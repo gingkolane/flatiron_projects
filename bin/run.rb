9cbcebf6b2898c39629572c28adc70bb88002aae
Wei <theweifeng@gmail.com>
2019-05-09 17:09:50 -0400

    Create models

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,16 @@
-require_relative 'config/environment'
+require_relative '../config/environment'
+require 'tty-prompt'
+require 'colorize'
+require 'faker'
+require 'artii'
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
+require_relative '../app/models/cli.rb'
+require_relative '../app/models/user.rb'
+require_relative '../app/models/investigator.rb'
+require_relative '../app/models/condition.rb'
+require_relative '../app/models/appointment.rb'
+
+CommandLineInterface.start_cli
