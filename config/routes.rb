e7bb46a7e8f340515839fb9d829b5023dea932f7
Chloe <gingkolane@gmail.com>
2019-06-17 05:57:11 -0400

    create model routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :conditions
+  resources :interventions
+  resources :sponsors
+  resources :investigators
+  resources :studies
+  
 end
