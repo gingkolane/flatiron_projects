8e0f1959e695d41a16d8b3242f6c5d784c596d1b
Chloe <gingkolane@gmail.com>
2019-06-20 12:17:29 -0400

    delete extra scaffold files and actions, adjust index fields

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,7 +20,7 @@ class ListsController < ApplicationController
   def create
     # create a new list with a list name and a user_id
     @list = List.create(name: list_params[:name], user_id: @current_user.id)
-    # create investigator_lists with this @list.id
+    # create investigators with these nested list_params. Have to convert to integer because params are all strings
     @investigators = params[:list][:investigator_ids].map {|id| id.to_i }
 
     @investigators.each do |investigator|
@@ -32,8 +32,6 @@ class ListsController < ApplicationController
   end
 
 
-  # PATCH/PUT /lists/1
-  # PATCH/PUT /lists/1.json
   def update
     @list = List.find(params[:id])
     @list.update(list_params)
@@ -41,30 +39,18 @@ class ListsController < ApplicationController
 
   end
 
-
-  # def add_to_list
-  # end
-
-  # DELETE /lists/1
-  # DELETE /lists/1.json
   def destroy
     @list.destroy
-    respond_to do |format|
-      format.html { redirect_to lists_url, notice: 'List was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to lists_url, notice: 'List was successfully destroyed.' 
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def find_list
       @list = List.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def list_params
       params.require(:list).permit(:name, :user_id, :investigator_ids)
     end
-
-
+    
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,50 +1,33 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
   def index
     @users = User.all
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1
   def show
-
   end
 
-  # POST /users
   def create
     @user = User.create(user_params)
     redirect_to user_path(@user)
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # PATCH/PUT /users/1
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    @user.update(user_params)
+    redirect_to @user, notice: 'User was successfully updated.' 
   end
 
-  # DELETE /users/1
   def destroy
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
+    redirect_to users_url, notice: 'User was successfully destroyed.' }
     end
   end
 
