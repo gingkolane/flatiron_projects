d8bf0374d1909c8291a2057ed0aa9099a13bfe35
Chloe <gingkolane@gmail.com>
2019-07-11 11:19:36 -0400

    add native page files

--- completion-api/app/controllers/completions_controller.rb
+++ completion-api/app/controllers/completions_controller.rb
@@ -1,45 +1,28 @@
 class CompletionsController < ApplicationController
   before_action :set_completion, only: [:show, :update, :destroy]
-  # skip_before_action :verify_authenticity_token
 
   def index
     @completions = Completion.all
-
     render json: @completions, include: [:repo, :user]
   end
 
   def show
     render json: @completion, include: [:repo, :user]
-
   end
 
-  # POST /completions
   def create
     @completion = Completion.new(completion_params)
-    # byebug
-    if @completion.save
-      render json: @completion, include: [:repo, :user], status: :created, location: @completion
-    else
-      render json: @completion.errors, status: :unprocessable_entity
-    end
-  end
+    @completion.save
 
+    render json: @completion, include: [:repo, :user], status: :created, location: @completion
+  end
 
   def update
-    if @completion.update(completion_params)
-      render json: @completion
-    else
-      render json: @completion.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /completions/1
-  def destroy
-    @completion.destroy
+    @completion.update(completion_params)
+    render json: @completion
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_completion
       @completion = Completion.find(params[:id])
     end
